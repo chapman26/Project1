b621bcfc37ae76153d8cb10eb9be71f36bcbcfa1
chapman26 <155932606+chapman26@users.noreply.github.com>
2024-01-08 11:57:53 -0500

    Update TestDemo.java

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -5,13 +5,7 @@ public class TestDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Task 1 testing");
-	}
-
-	public static void test(String heyPush, String heyPull){
-		heyPush = "It's working";
-		heyPull = "Right back at you";
-
-		System.out.println(heyPush + heyPull);
+		System.out.println("Task 2 testing pull");
 	}
 
 }
