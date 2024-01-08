1319430217bf4fdf70f7993d3c5b9096babda65b
samchapman <samchapman@192.168.1.245>
2024-01-08 15:06:10 -0500

    added code

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -7,6 +7,9 @@ public class TestDemo {
 		
 		System.out.println("Task 1 testing");
 		System.out.println("Task 2 testing pull");
+		System.out.println("Today is not Tuesday");
+		
+		System.out.println("Practice on my own");
 	}
 
 	//Today is Friday
